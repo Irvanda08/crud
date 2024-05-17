264ca64903e35853bd13324fb13253af4f2f6a5d
Benedictus Irvanda Nugroho <irvandanugroho08@gmail.com>
2024-05-17 20:07:28 +0700

    Update dataRoute.js

--- route/dataRoute.js
+++ route/dataRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const dataController = require("../controller/controllerData");
 const authMiddleware = require("../middleware/authMiddleware");
 
-router.get("/data", authMiddleware, dataController.findAll);
+router.get("/data", dataController.findAll);
 router.post("/data", authMiddleware, dataController.create);
 router.get("/data/:id", authMiddleware, dataController.findById);
 router.put("/data/:id", authMiddleware, dataController.update);
