b10a25b7c2495c2a850ce06fac4340bf4fec7ecc
Benedictus Irvanda Nugroho <irvandanugroho08@gmail.com>
2024-05-17 19:50:11 +0700

    Update index.js

--- index.js
+++ index.js
@@ -8,8 +8,15 @@ app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(204);
+  } else {
+    next();
+  }
 });
+
 app.use(bodyParser.json());
 
 const port = process.env.PORT || 8080;
