b280eed67f117468eef9ba9ff5c515b052136598
Benedictus Irvanda Nugroho <irvandanugroho08@gmail.com>
2024-05-17 16:58:03 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,15 @@ const bodyParser = require("body-parser");
 const dataRoutes = require("./route/dataRoute.js");
 
 const app = express();
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 8080;
 
 app.use(bodyParser.urlencoded({ extended: true }));
